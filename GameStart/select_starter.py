51188e46367a4541205dcaae4a4ee5ed3c719970
ErwinPrograms <erwinprograms@gmail.com>
2024-05-16 09:11:52 -0700

    Update GenericScene with an instantiation method for components

--- GameStart/select_starter.py
+++ GameStart/select_starter.py
@@ -5,12 +5,7 @@ from utilities.player_info import Player
 import combat.emotions
 class SelectStarterScene(GenericScene):
 
-    def __init__(self, display: pygame.Surface, game_state_object, player_info = None) -> None:
-        """Initializes the object with a PyGame surface to render and data from the game session"""
-        self.display = display
-        self.game_state_object = game_state_object
-        self.player_info = player_info
-
+    def create_components(self):
         start_img = pygame.image.load("start/start_button.png").convert_alpha()
         self.starter_button = Button(self.display, 500, 500, start_img, 0.4)
 
--- start/start_scene.py
+++ start/start_scene.py
@@ -4,14 +4,9 @@ import pygame
 from start.button import Button
 from utilities.generic_scene import GenericScene
 
-class StartScene(GenericScene):
+class StartScene(GenericScene):    
 
-    def __init__(self, display: pygame.Surface, game_state_object, player_info = None) -> None:
-        """Initializes the object with a PyGame surface to render and data from the game session"""
-        self.display = display
-        self.game_state_object = game_state_object
-        self.player_info = player_info
-
+    def create_components(self):
         start_img = pygame.image.load("start/start_button.png").convert_alpha()
         self.start_button = Button(self.display, 100, 100, start_img, 0.4)
 
