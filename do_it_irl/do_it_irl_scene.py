80a492a84b696da50806c962e680e0a5622c957a
Marinus Lentile <marinuslentile3@gmail.com>
2024-05-17 13:00:11 +0200

    wip do_it_irl_scene, add buttons for timer

--- do_it_irl/do_it_irl_scene.py
+++ do_it_irl/do_it_irl_scene.py
@@ -3,6 +3,7 @@ from utilities.game_state_object import GameStateObject
 from utilities.generic_scene import GenericScene
 from utilities.player_info import Player
 from do_it_irl.timer import Timer
+from start.button import Button
 
 class DoItIRLScene(GenericScene):
 
@@ -26,26 +27,40 @@ class DoItIRLScene(GenericScene):
         self.instruction_image = None
         self.initialized = False
 
+    def create_components(self) -> None:
+        # start button
+        start_img = pygame.image.load("do_it_irl/assets/start_button.png").convert_alpha()
+        self.starter_button = Button(self.display, self.WIDTH//2-100, 200, start_img, 0.8)
+        # finish button
+        finish_img = pygame.image.load("do_it_irl/assets/finish_button.png").convert_alpha()
+        self.finish_button = Button(self.display, self.WIDTH//2+100, 200, finish_img, 0.8)
+
     def game_body_loop(self) -> None:
         # init task and timer once
         if not self.initialized:
             self.initialize()
             self.initialized = True
 
-        # todo: start button and finished button
+        if self.starter_button.activated:
+            self.timer.start()
+            
 
         instruction_image_path = "do_it_irl/assets/" + self.task + "-instruction.png"
         self.instruction_image = pygame.image.load(instruction_image_path)
+        self.instruction_image_rect = self.instruction_image.get_rect(center=(self.WIDTH/2, 400))
 
         self.draw()
         
 
     def initialize(self):
+        # set the task id
         self.task = self.player_info.task
-        self.timer = Timer(self.task_parameters[self.task], (604-40, 40), 70)
-        self.timer.start()
+        # set up timer
+        self.timer = Timer(self.task_parameters[self.task], (self.WIDTH//2, 70), 70)
 
     def draw(self):
         self.display.fill("violet")
-        self.display.blit(self.instruction_image, (604-100, 200))
+        self.display.blit(self.instruction_image, self.instruction_image_rect)
         self.timer.draw(self.display)
+        self.starter_button.draw()
+        self.finish_button.draw()
