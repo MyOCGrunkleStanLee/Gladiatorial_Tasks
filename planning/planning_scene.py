af3640b5d455913c40dde8feda9984a651fa7e1a
ErwinPrograms <erwinprograms@gmail.com>
2024-05-20 13:48:19 -0700

    Fix: planning now has clickable buttons
    Updated the bordered_task image to have a low opacity layer in the center. This makes is so that pygame detects collisions with the center of that image

--- planning/planning_scene.py
+++ planning/planning_scene.py
@@ -11,6 +11,7 @@ class PlanningScene(GenericScene):
             "Assets/BreakItDownScreen.png"
         ).convert_alpha()
 
+        # selectable tasks as strings
         task_strings = [
             '10 Push Ups',
             '10 Pull Ups',
@@ -26,17 +27,40 @@ class PlanningScene(GenericScene):
             '10 Burpees',
         ]
 
-        task_positions = []
+        # (x, y) coordinate tules based on topleft positioning
+        task_positions = [
+            (615, 75),
+            (615, 155),
+            (615, 225),
+            (615, 305),
+            (615, 388),
+            (615, 465),
+            (940, 75),
+            (940, 155),
+            (940, 225),
+            (940, 305),
+            (940, 388),
+            (940, 465)
+        ]
 
         self.bordered_task = pygame.image.load("planning/border_task.png").convert_alpha()
-        self.push_up_button = Button(
-            self.display, 616, 76, self.bordered_task, 1, "topleft"
-        )
+        
+        self.task_buttons = []
+        for x_position, y_position in task_positions:
+            created_button = Button(
+                self.display, x_position, y_position, self.bordered_task, 1, "topleft"
+            )
+            self.task_buttons.append(created_button)
 
     def game_body_loop(self) -> None:
 
+        for button in self.task_buttons:
+            if button.activated:
+                print(button)
+
         self.display.blit(self.background_img, (0, 0))
-        self.push_up_button.draw()
+        for button in self.task_buttons:
+            button.draw()
 
     def next_screen(self) -> None:
         self.game_state_object.current_state = "select_starter"
