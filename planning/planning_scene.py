1bb98558e3f19120bdeb4d4a6aacf452fe5f65c8
ErwinPrograms <erwinprograms@gmail.com>
2024-05-20 13:28:39 -0700

    Refactor: change planning scene layout to be BreakItDown selector
    Change the layout of the planning scene to use the BreakItDownScreen.png asset. This requires changing functionality such that the player can click on each button and add it to the list

--- planning/planning_scene.py
+++ planning/planning_scene.py
@@ -1,46 +1,42 @@
 import pygame
 from utilities.generic_scene import GenericScene
 from planning.input_box import InputBox
+from start.button import Button
 
 
 class PlanningScene(GenericScene):
 
     def create_components(self) -> None:
-        self.prompt_list = [
-            "How many Push Ups?",
-            "How many sit ups?",
-            "How many squats?",
-            "How many seconds plank?",
-            "How many pull-ups?",
+        self.background_img = pygame.image.load(
+            "Assets/BreakItDownScreen.png"
+        ).convert_alpha()
+
+        task_strings = [
+            '10 Push Ups',
+            '10 Pull Ups',
+            '1-Minute Plank',
+            '10 Sit Ups',
+            '10 Squats',
+            '10 Crunches',
+            '10 Lunges',
+            '20 High Knees',
+            '10 Incline Rows',
+            '10 Calf Raises',
+            '10 Jumping Jacks',
+            '10 Burpees',
         ]
 
-        my_font = pygame.font.SysFont('College', 30)
-        self.text_surface = my_font.render('Gladiatorial Tasks', False, (0, 0, 0))
+        task_positions = []
 
-        CENTERED_DIMENSIONS = (
-            340, #x_position
-            120, #y_position
-            600, #width
-            400, #length
-        )  # These measurements create a centered rectangle for a screen WIDTH, HEIGHT = 1280, 620
-
-        self.input_box = InputBox(self.display, *CENTERED_DIMENSIONS, self.prompt_list[0])
+        self.bordered_task = pygame.image.load("planning/border_task.png").convert_alpha()
+        self.push_up_button = Button(
+            self.display, 616, 76, self.bordered_task, 1, "topleft"
+        )
 
     def game_body_loop(self) -> None:
-        self.display.fill("blue")
 
-        keydown_event = self.game_state_object.keydown_event
+        self.display.blit(self.background_img, (0, 0))
+        self.push_up_button.draw()
 
-        if keydown_event != None:
-            if keydown_event.key == pygame.K_BACKSPACE:
-                self.input_box.user_text = self.input_box.user_text[:-1]
-            else:
-                self.input_box.user_text += keydown_event.unicode
-
-            self.game_state_object.keydown_event = None
-            
-
-        self.input_box.draw()
-    
     def next_screen(self) -> None:
         self.game_state_object.current_state = "select_starter"
