f0174c98a852e096000252910a3176696a8ffb7f
ErwinPrograms <erwinprograms@gmail.com>
2024-05-22 08:48:28 -0700

    Resolve merge conflict with main

--- GameStart/select_starter.py
+++ GameStart/select_starter.py
@@ -2,28 +2,52 @@ import pygame
 from start.button import Button
 from utilities.generic_scene import GenericScene
 from utilities.player_info import Player
+from GameStart.starter_option import StarterOption
 import combat.emotions
 
 
 class SelectStarterScene(GenericScene):
-    
+
     def create_components(self):
         start_img = pygame.image.load("start/start_button.png").convert_alpha()
-        self.starter_button = Button(self.display, self.WIDTH/2, 300, start_img, 0.4)
+        self.starter_button = Button(self.display, self.WIDTH / 2, 500, start_img, 0.4)
 
-        #Title
+        # Title
         my_font = pygame.font.SysFont('College', 30)
         self.text_surface = my_font.render('Select Starter', False, (0, 0, 0))
+
+        happiness_image = pygame.image.load(
+            "Assets/ChooseTaskScreenLargeSquare.png"
+        )  # placeholder
+        self.starter_option = StarterOption(
+            self.display, 560, 100, happiness_image, "Happiness"
+        )
+
+        self.selected_starter = ''
 
     def game_body_loop(self):
         self.display.fill("green")
 
         # Title
-        self.display.blit(self.text_surface, (self.WIDTH/2-self.text_surface.get_width()/2, 50))
+        self.display.blit(
+            self.text_surface, (self.WIDTH / 2 - self.text_surface.get_width() / 2, 50)
+        )
 
         self.starter_button.draw(self.display)
+        self.starter_option.draw()
+
+        if self.starter_option.activated:
+            # print(f"Selected {self.starter_option.emotion_name}")
+            self.selected_starter = self.starter_option.emotion_name
 
         # change game state on click
         if self.starter_button.activated:
-            self.player_info.add_emotion(combat.player.happiness)
-            self.game_state_object.current_state = "combat"
+            self.next_screen()
+
+    def next_screen(self):
+        if not self.selected_starter:
+            return
+
+        #hardcoded happiness passed
+        self.player_info.add_emotion(combat.player.happiness)
+        self.game_state_object.current_state = "combat"
